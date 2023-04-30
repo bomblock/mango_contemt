fde38cc4d8a8e69f70a170a254ad79b97b2f5b17
bomblock <ekwotmd@gmail.com>
2023-04-30 14:56:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mango_contemt/SplashActivity1.kt
@@ -0,0 +1,11 @@
+package com.example.mango_contemt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+    }
+}
